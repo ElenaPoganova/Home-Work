b15371dbe65b0ecea3d6e9a955d7e229f5a371e4
Elena Poganova <a.n.rus@mail.ru>
2023-01-14 17:52:12 +0400

    Добавила папку Work2

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine($"{a} == max {b} == min");
+else if (a < b)
+    Console.WriteLine($"{a} == min {b} == max");
+else 
+    Console.WriteLine($"{a} = {b}");
