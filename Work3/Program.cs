ce6c5dd2bb093cd1b212aafb4a854b1d9955ff36
Elena Poganova <a.n.rus@mail.ru>
2023-01-16 14:39:49 +0400

    Внесены изменения в папку Work3

--- Work3/Program.cs
+++ Work3/Program.cs
@@ -5,18 +5,11 @@ Console.Write("Введите 2-ое число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 3-е число: ");
 int c = Convert.ToInt32(Console.ReadLine());
-while ( (a = b) || (b = c) || (a = c) )
-{
-    Console.Write("Числа равны между собой!\nВведите 1-ое число: ");
-    a = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите 2-ое число: ");
-    b = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите 3-е число: ");
-    с = Convert.ToInt32(Console.ReadLine());
-}
 if (a > b && a > c)
     Console.WriteLine($"{a} == max");
 else if (b > a && b > c)
     Console.WriteLine($"{b} == max");
-else 
+else if (c > a && c > b)
     Console.WriteLine($"{c} == max");
+else 
+    Console.WriteLine("Значения равны");
