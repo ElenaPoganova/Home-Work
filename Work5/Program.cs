59e4a140dbf5325735b2e7a370abbd6a79b4ad76
Elena Poganova <a.n.rus@mail.ru>
2023-01-16 14:42:54 +0400

    Внесены изменения в папку Work5

--- Work5/Program.cs
+++ Work5/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n / n;
+while (m <= n)
+{
+   if (m % 2 == 0)
+    Console.Write($"{m}");
+    m = m + 1;
+}
